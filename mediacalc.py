c228838b4e612355c82b0982561242d825315153
Fabrício Barbosa <108180400+seufabricio@users.noreply.github.com>
2023-07-11 15:23:38 -0300

    Add files via upload

---
+++ mediacalc.py
@@ -0,0 +1,5 @@
+a = int(input("Digite o primeiro número: "))
+b = int(input("Digite o segundo número: "))
+c = int(input("Digite o terceiro número: "))
+media = (a + b + c) / 3
+print("A média é: {:.2f}".format(media))
